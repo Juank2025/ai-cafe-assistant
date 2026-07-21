e7a99f74b7d52c126edcdd0444b7e1e0b027e67a
Juan Carlos Palma <juankdreamer@gmail.com>
2026-07-21 01:57:42 -0400

    Agregado proxy de PythonAnywhere

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -11,41 +11,31 @@ from src.config import TELEGRAM_TOKEN
 from src.agent import CafeteriaAgent
 
 
-
 class TelegramBot:
 
-
     def __init__(self, vectorstore):
-
         self.agent = CafeteriaAgent(vectorstore)
-
 
     async def responder(
         self,
         update: Update,
         context: ContextTypes.DEFAULT_TYPE
     ):
-
         mensaje = update.message.text
-
 
         respuesta = self.agent.preguntar(
             mensaje
         )
 
-
         await update.message.reply_text(
             respuesta
         )
-
-
 
     async def inicio(
         self,
         update: Update,
         context: ContextTypes.DEFAULT_TYPE
     ):
-
         await update.message.reply_text(
             """
 ☕ Bienvenido a Café Delicias del Norte.
@@ -62,14 +52,15 @@ Puedes preguntarme sobre:
 """
         )
 
-
-
     def ejecutar(self):
+        # URL del proxy requerido por la cuenta gratuita de PythonAnywhere
+        PROXY_URL = "http://proxy.server:3128"
 
         app = Application.builder()\
             .token(TELEGRAM_TOKEN)\
+            .proxy_url(PROXY_URL)\
+            .get_updates_proxy_url(PROXY_URL)\
             .build()
-
 
         app.add_handler(
             CommandHandler(
@@ -78,7 +69,6 @@ Puedes preguntarme sobre:
             )
         )
 
-
         app.add_handler(
             MessageHandler(
                 filters.TEXT,
@@ -86,8 +76,6 @@ Puedes preguntarme sobre:
             )
         )
 
-
         print("Bot Telegram iniciado...")
-
 
         app.run_polling()
